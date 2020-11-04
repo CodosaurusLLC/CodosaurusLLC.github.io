eb084465b87dd0dff85b2b748978ac6aef255866
Dave Aronson <davearonson@users.noreply.github.com>
2020-11-04 13:35:47 -0500

    simplify abv formula as other overestimates

--- assets/sum-of-a-batch/analyze.js
+++ assets/sum-of-a-batch/analyze.js
@@ -3,19 +3,8 @@ function mead_analyze() {
   var fg = get_number('FG', errs);
   var og = get_number('OG', errs);
 
-  var abw = 76.08 * (og - fg) / (1.775 - og);
-  var abv = abw * fg / 0.794;
+  var abv = (og - fg) * 131.25;
 
-  document.getElementById('results').innerHTML = [
-    '<table>',
-      '<tr>',
-        '<td>Alcohol by Volume: </td>',
-        '<td>', abv.toFixed(1), '%</td>',
-      '</tr>',
-      '<tr>',
-        '<td>Alcohol by Weight: </td>',
-        '<td>', abw.toFixed(1), '%</td>',
-      '</tr>',
-    '</table>'
-  ].join('');
+  document.getElementById('results').innerHTML =
+    '<p>That means an ABV of about ' + abv.toFixed(1) + '%.</p>';
 }
